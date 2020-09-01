dee45a7fc423a9b335bea834b7c831ac1ad1abd4
Tuuz <2539@aerofsx.com>
2020-09-01 16:01:54 +0800

    Tuuz

--- config/url.go
+++ config/url.go
@@ -1,11 +1,11 @@
 package config
 
 const AuthURL = "/v1/index/index/userauthh"
-const Msg_list = "v1/message/message/list"
+const Msg_list = "/v1/message/message/list"
 
 const Private_msg = "/api/msg/msg"
 const Group_msg = "/api/msg/group_msg"
-const Request_count = "/api/req/count"
+const Request_count = "/v1/request/request/count"
 
 const ManualAPI = "/api/msg/"
 
