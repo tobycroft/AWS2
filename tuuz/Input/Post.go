41ccab9acff438a38d0d38d0340cff2eb282de00
Tuuz <2539@aerofsx.com>
2020-09-03 15:24:38 +0800

    Tuuz

--- tuuz/Input/Post.go
+++ tuuz/Input/Post.go
@@ -2,25 +2,31 @@ package Input
 
 import (
 	"github.com/gin-gonic/gin"
+	"html/template"
 	"main.go/tuuz/Calc"
+	"main.go/tuuz/Jsong"
 	"main.go/tuuz/RET"
 )
 
 func Post(key string, c *gin.Context, xss bool) (string, bool) {
 	in, ok := c.GetPostForm(key)
 	if !ok {
-		c.JSON(RET.Ret_fail(400, key, key))
+		c.JSON(RET.Ret_fail(400, key, "POST-["+key+"]"))
 		c.Abort()
 		return "", false
 	} else {
-		return in, true
+		if xss {
+			return template.JSEscapeString(in), true
+		} else {
+			return in, true
+		}
 	}
 }
 
 func PostInt(key string, c *gin.Context) (int, bool) {
 	in, ok := c.GetPostForm(key)
 	if !ok {
-		c.JSON(RET.Ret_fail(400, key, key))
+		c.JSON(RET.Ret_fail(400, key, "POST-["+key+"]"))
 		c.Abort()
 		return 0, false
 	} else {
@@ -37,7 +43,7 @@ func PostInt(key string, c *gin.Context) (int, bool) {
 func PostInt64(key string, c *gin.Context) (int64, bool) {
 	in, ok := c.GetPostForm(key)
 	if !ok {
-		c.JSON(RET.Ret_fail(400, key, key))
+		c.JSON(RET.Ret_fail(400, key, "POST-["+key+"]"))
 		c.Abort()
 		return 0, false
 	} else {
@@ -54,7 +60,7 @@ func PostInt64(key string, c *gin.Context) (int64, bool) {
 func PostFloat64(key string, c *gin.Context) (float64, bool) {
 	in, ok := c.GetPostForm(key)
 	if !ok {
-		c.JSON(RET.Ret_fail(400, key, key))
+		c.JSON(RET.Ret_fail(400, key, "POST-["+key+"]"))
 		c.Abort()
 		return 0, false
 	} else {
@@ -63,6 +69,66 @@ func PostFloat64(key string, c *gin.Context) (float64, bool) {
 			c.JSON(RET.Ret_fail(407, key+" should be float64", key+" should be float64"))
 			c.Abort()
 			return 0, false
+		}
+		return i, true
+	}
+}
+
+func PostBool(key string, c *gin.Context) (bool, bool) {
+	in, ok := c.GetPostForm(key)
+	if !ok {
+		c.JSON(RET.Ret_fail(400, key, "POST-["+key+"]"))
+		c.Abort()
+		return false, false
+	} else {
+		switch in {
+		case "1":
+			return true, true
+
+		case "0":
+			return false, true
+
+		case "true":
+			return true, true
+
+		case "false":
+			return false, true
+
+		default:
+			return false, false
+		}
+	}
+}
+
+func PostArray(key string, c *gin.Context) ([]interface{}, bool) {
+	in, ok := c.GetPostForm(key)
+	if !ok {
+		c.JSON(RET.Ret_fail(400, key, "POST-["+key+"]"))
+		c.Abort()
+		return nil, false
+	} else {
+		i, err := Jsong.JArray(in)
+		if err != nil {
+			c.JSON(RET.Ret_fail(407, key+" should be a Json-Array", key+" should be a Json-Array"))
+			c.Abort()
+			return nil, false
+		}
+		return i, true
+	}
+}
+
+func PostObject(key string, c *gin.Context) (map[string]interface{}, bool) {
+	in, ok := c.GetPostForm(key)
+	if !ok {
+		c.JSON(RET.Ret_fail(400, key, "POST-["+key+"]"))
+		c.Abort()
+		return nil, false
+	} else {
+		i, err := Jsong.JObject(in)
+		if err != nil {
+			c.JSON(RET.Ret_fail(407, key+" should be a Json-Object", key+" should be a Json-Object"))
+			c.Abort()
+			return nil, false
 		}
 		return i, true
 	}
