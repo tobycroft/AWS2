c40450b42602e3aa7160ebfc5275b4b682169dc8
Tuuz <miracle-plus@outlook.com>
2020-12-01 15:15:51 +0800

    Tuuz

--- function/http/http.go
+++ function/http/http.go
@@ -3,7 +3,6 @@ package http
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
 	"main.go/config"
 	"main.go/function/ws"
 	"main.go/tuuz/Calc"
@@ -95,10 +94,10 @@ func json_handler(c *gin.Context, json map[string]interface{}, to_users []interf
 	switch Type {
 	case "system":
 		for _, uid := range to_users {
-			conn, has := ws.User2Conn2.Load(Calc.Any2String(uid))
+			ch, has := ws.User2Chan2.Load(Calc.Any2String(uid))
 			if has {
 				uids = append(uids, uid)
-				conn.(*websocket.Conn).WriteJSON(data)
+				ch.(chan interface{}) <- data
 			} else {
 				uidf = append(uidf, uid)
 			}
@@ -107,10 +106,10 @@ func json_handler(c *gin.Context, json map[string]interface{}, to_users []interf
 
 	case "refresh_list":
 		for _, uid := range to_users {
-			conn, has := ws.User2Conn2.Load(Calc.Any2String(uid))
+			ch, has := ws.User2Chan2.Load(Calc.Any2String(uid))
 			if has {
 				uids = append(uids, uid)
-				conn.(*websocket.Conn).WriteJSON(data)
+				ch.(chan interface{}) <- data
 			} else {
 				uidf = append(uidf, uid)
 			}
@@ -119,10 +118,10 @@ func json_handler(c *gin.Context, json map[string]interface{}, to_users []interf
 
 	case "private_chat":
 		for _, uid := range to_users {
-			conn, has := ws.User2Conn2.Load(Calc.Any2String(uid))
+			ch, has := ws.User2Chan2.Load(Calc.Any2String(uid))
 			if has {
 				uids = append(uids, uid)
-				conn.(*websocket.Conn).WriteJSON(data)
+				ch.(chan interface{}) <- data
 			} else {
 				uidf = append(uidf, uid)
 			}
@@ -131,10 +130,10 @@ func json_handler(c *gin.Context, json map[string]interface{}, to_users []interf
 
 	case "group_chat":
 		for _, uid := range to_users {
-			conn, has := ws.User2Conn2.Load(Calc.Any2String(uid))
+			ch, has := ws.User2Chan2.Load(Calc.Any2String(uid))
 			if has {
 				uids = append(uids, uid)
-				conn.(*websocket.Conn).WriteJSON(data)
+				ch.(chan interface{}) <- data
 			} else {
 				uidf = append(uidf, uid)
 			}
@@ -143,10 +142,10 @@ func json_handler(c *gin.Context, json map[string]interface{}, to_users []interf
 
 	case "request_count":
 		for _, uid := range to_users {
-			conn, has := ws.User2Conn2.Load(Calc.Any2String(uid))
+			ch, has := ws.User2Chan2.Load(Calc.Any2String(uid))
 			if has {
 				uids = append(uids, uid)
-				conn.(*websocket.Conn).WriteJSON(data)
+				ch.(chan interface{}) <- data
 			} else {
 				uidf = append(uidf, uid)
 			}
@@ -155,10 +154,10 @@ func json_handler(c *gin.Context, json map[string]interface{}, to_users []interf
 
 	case "push":
 		for _, uid := range to_users {
-			conn, has := ws.User2Conn2.Load(Calc.Any2String(uid))
+			ch, has := ws.User2Chan2.Load(Calc.Any2String(uid))
 			if has {
 				uids = append(uids, uid)
-				conn.(*websocket.Conn).WriteJSON(data)
+				ch.(chan interface{}) <- data
 			} else {
 				uidf = append(uidf, uid)
 			}
@@ -167,20 +166,20 @@ func json_handler(c *gin.Context, json map[string]interface{}, to_users []interf
 
 	case "message":
 		for _, uid := range to_users {
-			conn, has := ws.User2Conn2.Load(Calc.Any2String(uid))
+			ch, has := ws.User2Chan2.Load(Calc.Any2String(uid))
 			if has {
 				uids = append(uids, uid)
-				conn.(*websocket.Conn).WriteJSON(data)
+				ch.(chan interface{}) <- data
 			}
 		}
 		break
 
 	case "indoor_message":
 		for _, uid := range to_users {
-			conn, has := ws.User2Conn2.Load(Calc.Any2String(uid))
+			ch, has := ws.User2Chan2.Load(Calc.Any2String(uid))
 			if has {
 				uids = append(uids, uid)
-				conn.(*websocket.Conn).WriteJSON(data)
+				ch.(chan interface{}) <- data
 			} else {
 				uidf = append(uidf, uid)
 			}
@@ -189,10 +188,10 @@ func json_handler(c *gin.Context, json map[string]interface{}, to_users []interf
 
 	case "outer_all":
 		for _, uid := range to_users {
-			conn, has := ws.User2Conn2.Load(Calc.Any2String(uid))
+			ch, has := ws.User2Chan2.Load(Calc.Any2String(uid))
 			if has {
 				uids = append(uids, uid)
-				conn.(*websocket.Conn).WriteJSON(data)
+				ch.(chan interface{}) <- data
 			} else {
 				uidf = append(uidf, uid)
 			}
@@ -214,10 +213,10 @@ func json_handler(c *gin.Context, json map[string]interface{}, to_users []interf
 	default:
 		fmt.Println("default")
 		for _, uid := range to_users {
-			conn, has := ws.User2Conn2.Load(Calc.Any2String(uid))
+			ch, has := ws.User2Chan2.Load(Calc.Any2String(uid))
 			if has {
 				uids = append(uids, uid)
-				conn.(*websocket.Conn).WriteJSON(data)
+				ch.(chan interface{}) <- data
 			} else {
 				uidf = append(uidf, uid)
 			}
